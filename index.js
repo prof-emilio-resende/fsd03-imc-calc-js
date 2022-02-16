f91064abc7042721b2a1ca72a587cc60d3cd5927
Emilio Murta <emilio.murta@msitbrasil.com>
2022-02-16 19:50:43 -0300

    prototype

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-var altura = 0.00;
-var peso = 0.00;
-
-function capturarValores() {
-  altura = parseFloat(document.getElementById('altura').value);
-  peso = parseFloat(document.getElementById('peso').value);
+function Person(height, weight) {
+  if (typeof(height) !== 'number' || isNaN(height))
+    throw Error('Height is not valid as a number...');
+  if (typeof(weight) !== 'number' || isNaN(weight))
+    throw Error('Weight is not valid as a number...');
+  
+  this._height = height;
+  this._weight = weight;
+  this.getHeight = function() {
+    return this._height;
+  }
+  this.getWeight = function() {
+    return this._weight;
+  }
 }
 
-function calcularImc() {
-  return peso / altura ** 2;
+function Dietician(height, weight) {
+  Person.call(this, height, weight);
+  this.calculateImc = function() {
+    return this.getWeight() / this.getHeight() ** 2;
+  }
+}
+Dietician.prototype = Object.create(Person.prototype);
+Dietician.prototype.constructor = Dietician;
+
+
+function createDieticianFromForm() {
+  var height = parseFloat(document.getElementById('height').value);
+  var weight = parseFloat(document.getElementById('weight').value);
+  
+  return new Dietician(height, weight);
 }
 
 function imprimirResultado(resultado) {
   document.getElementById('imc').innerHTML = resultado;
 }
 
-function calcularEImprimirIMC() {
-  capturarValores();
-  resultado = calcularImc();
+function calculate() {
+  var dietician = createDieticianFromForm();
+  resultado = dietician.calculateImc();
   imprimirResultado(resultado);
 }
